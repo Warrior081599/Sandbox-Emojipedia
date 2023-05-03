61c1fe7a8edf8494aa6200af905a4bad1a18a184
Azad Raj <113576074+Warrior081599@users.noreply.github.com>
2023-05-03 12:31:35 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,82 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "🥹",
+    name: "Face Holding Back Tears",
+    meaning:
+      "The 🥹 emoji, which shows a slightly smiling face with tears in its eyes, can express various emotions such as gratitude, emotion, or pride"
+  },
+
+  {
+    id: 5,
+    emoji: "😅",
+    name: "Smiling Face With Open Mouth & Cold Sweat",
+    meaning:
+      "A grinning face with drops of sweat is represented by the 😅 emoji. This is used to express relief that a tricky situation has been successfully handled or just gone well, such as an important meeting."
+  },
+
+  {
+    id: 6,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning:
+      "The 😂 emoji, which represents a laughing face with tears of joy, expresses that you can barely hold yourself on the chair with laughter"
+  },
+
+  {
+    id: 7,
+    emoji: "🥲",
+    name: "Smiling face with tear",
+    meaning:
+      "The 🥲 emoji stands for smiling through the tough times and symbolizes trying to stay positive even in difficult moments. "
+  },
+
+  {
+    id: 8,
+    emoji: "😊",
+    name: "miling face With Smiling Eyes",
+    meaning:
+      "With a shy, embarrassed smile, the 😊 emoji radiates happiness, contentment, peace of mind, and gratitude. The bright eyes and red cheeks that characterize this emoji symbolize joy and warmth."
+  },
+
+  {
+    id: 9,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "A heavenly smile is shown by the 😇 halo emoji, which symbolizes innocence and goodness. People use it to express their good deeds, loving behavior or exemplary attitude"
+  },
+
+  {
+    id: 10,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "This slightly smiling emoji expresses a pleasant contentment and inner peace. It is used to make a statement seem friendlier, even if it is not necessarily meant that way."
+  }
+];
+
+export default emojipedia;
